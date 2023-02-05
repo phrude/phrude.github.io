b869bd2bdd5006a51b2cb9d0b40e12c5dd090713
Hi120ki <12624257+hi120ki@users.noreply.github.com>
2023-02-05 22:01:44 +0900

    fix projectName
    Signed-off-by: Hi120ki <12624257+hi120ki@users.noreply.github.com>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -19,7 +19,7 @@ const config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'phrude', // Usually your GitHub org/user name.
-  projectName: 'doc', // Usually your repo name.
+  projectName: 'phrude.github.io', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
